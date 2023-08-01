6f1973b60f593b6a406c7c59618dcbfabc3a8e9b
chayanbansal9195 <chayanbansal1999@gmail.com>
2023-08-01 21:47:13 +0530

    git initial code setup changes

---
+++ src/main/java/com/kafka/cbsal/real/SpringbootKafkaWikimediaProjectCbsalApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.cbsal.real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaWikimediaProjectCbsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaWikimediaProjectCbsalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/cbsal/real/SpringbootKafkaWikimediaProjectCbsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.cbsal.real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaProjectCbsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
