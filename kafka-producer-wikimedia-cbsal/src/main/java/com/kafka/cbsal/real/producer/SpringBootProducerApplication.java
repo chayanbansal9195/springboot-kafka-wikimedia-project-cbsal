5d0c3e550984a2860173abeaacbdbf8fecbf89eb
chayanbansal9195 <chayanbansal1999@gmail.com>
2023-08-01 22:43:58 +0530

    added springboot producer module in main module

---
+++ kafka-producer-wikimedia-cbsal/src/main/java/com/kafka/cbsal/real/producer/SpringBootProducerApplication.java
@@ -0,0 +1,12 @@
+package com.kafka.cbsal.real.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication {
+	public static void main(String Args[]) {
+		SpringApplication.run(SpringBootProducerApplication.class);
+	}
+
+}
