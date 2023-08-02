ad2d8077a55fb534b1399725f8c3ff9680d50699
chayanbansal9195 <chayanbansal1999@gmail.com>
2023-08-02 11:33:35 +0530

    added wikimedia producer code cbsal

--- kafka-producer-wikimedia-cbsal/src/main/java/com/kafka/cbsal/real/producer/SpringBootProducerApplication.java
+++ kafka-producer-wikimedia-cbsal/src/main/java/com/kafka/cbsal/real/producer/SpringBootProducerApplication.java
@@ -1,12 +1,24 @@
 package com.kafka.cbsal.real.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kafka.cbsal.real.producer.service.WikimediaChangesProducerService;
+
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
 	public static void main(String Args[]) {
 		SpringApplication.run(SpringBootProducerApplication.class);
+	}
+
+	@Autowired
+	private WikimediaChangesProducerService wikimediaChangesProducerService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		wikimediaChangesProducerService.sendMessage();
 	}
 
 }
---
+++ kafka-producer-wikimedia-cbsal/src/main/java/com/kafka/cbsal/real/producer/service/WikimediaChangesProducerService.java
@@ -0,0 +1,41 @@
+package com.kafka.cbsal.real.producer.service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.kafka.cbsal.real.producer.handlers.WikimediaChangesHandlers;
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+
+@Service
+public class WikimediaChangesProducerService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducerService.class);
+
+	KafkaTemplate<String, String> kafkaTemplate;
+
+	public WikimediaChangesProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
+	public void sendMessage() throws InterruptedException {
+		String topic = "wikimedia_recent_change_topic";
+
+		// to read realtime stream data from wikimedia, we use event source
+		// we need to add couple of libraries
+
+		EventHandler eventHandler = new WikimediaChangesHandlers(kafkaTemplate, topic);
+		String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+		EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+		EventSource eventSource = builder.build();
+		eventSource.start();
+		
+		TimeUnit.SECONDS.sleep(10);
+	}
+
+}
